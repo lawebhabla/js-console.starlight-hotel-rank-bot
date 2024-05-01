0f7984a99234ed05775dbb175e6d42c388df9049
Jose Manuel Ortega <jmortega@vocento.com>
2024-05-01 11:48:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ejemplo de llamada
+http://localhost:8080/?userId=4018000780&rankId=1
+
+# IDs de usuarios de prueba
+4018000780 jmotfx 
+4149045181 castilloSamurai
+
+# npm start
---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const noblox = require("noblox.js");
+const express = require('express');
+const app = express();
+
+// Lectura de constantes de .env
+let cookie = process.env.COOKIE;
+let groupId = process.env.GROUP_ID;
+const PORT = process.env.PORT || 8080;
+
+noblox.setCookie(cookie).then(function(currentUser) {
+    console.log(`Logged in as ${currentUser.UserName}`);
+
+    app.get('/', async (req, res) => {
+        // Obtenemos los parametros userId y rankId
+        let userId = parseInt(req.query.userId);
+        let rankId = parseInt(req.query.rankId);
+    
+        // Validacion de parametros obligatorios de la consulta
+        if (isNaN(userId) || isNaN(rankId)) {
+            return res.status(400).json({ error: 'Los parametros userId y rankId son obligatorios' });
+        }
+        else {
+            noblox.setRank(groupId, userId, rankId)
+            .then(() => {
+                const message = `Establecido ranking ${rankId} para el usuario ${userId}`;
+                res.send(message);
+            })
+            .catch((error) => {
+                res.status(500).send(`Error al establecer el rango: ${error.message}`);
+            });
+        }
+    });
+
+    // Iniciar el servidor
+    app.listen(PORT, () => {
+        console.log(`Servidor escuchando en el puerto ${PORT}`);
+    });
+}).catch(function(err) {
+    console.log("Unable to log in!", err)
+})
